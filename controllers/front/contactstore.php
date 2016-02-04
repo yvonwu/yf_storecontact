7cfddb145b13dc4e984915064e0caa26bd1b920e
yvonwu <wuyifan2003@hotmail.com>
2016-02-04 16:37:54 +0100

    end dat save

--- controllers/front/contactstore.php
+++ controllers/front/contactstore.php
@@ -1,1 +1,15 @@
 <?php
+
+class Yf_storeContactContactStoreModuleFrontController extends ModuleFrontControllerCore {
+
+
+
+	 public function initContent() {
+        
+        parent::initContent();
+
+        $this->setTemplate('contactstore.tpl');
+
+    }
+
+}
